03a2e5e2786b24ed1c45ef96ad14aeb7eaa167c1
victor <victor>
2018-12-18 16:13:38 +0000

    Set up rustic and written some code.

---
+++ day1/src/lib.rs
@@ -0,0 +1,8 @@
+use std::io;
+
+pub fn compute_frequency<R>(reader: R) -> io::Result<i64>
+where
+    R: io::BufRead,
+{
+    Ok(0)
+}
--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,20 @@
+extern crate day1;
+
+use std::env::args;
+use std::fs::File;
+use std::io::BufReader;
+use std::process::exit;
+
 fn main() {
-    println!("Hello, world!");
+    let ar: Vec<String> = args().collect();
+    if ar.len() != 2 {
+        eprintln!("Usage: {} <INPUT>", &ar[0]);
+        exit(-1);
+    }
+
+    let input_file = File::open(&ar[1]).expect("Unable to open input file.");
+    let frequency =
+        day1::compute_frequency(BufReader::new(input_file)).expect("Failed to compute frequency.");
+
+    println!("Computed frequency: {}", frequency);
 }
