c3da337347f2339aceeb96fffd35519c62a79e0a
victor <victor>
2018-12-19 07:44:53 +0000

    Finished first task, need to refactor and write some tests.

--- day1/src/lib.rs
+++ day1/src/lib.rs
@@ -1,8 +1,21 @@
 use std::io;
 
-pub fn compute_frequency<R>(reader: R) -> io::Result<i64>
+pub fn compute_frequency<R>(reader: R) -> std::result::Result<i64, String>
 where
     R: io::BufRead,
 {
-    Ok(0)
+    let elements: std::result::Result<Vec<i64>, String> = reader
+        .lines()
+        .map(|s| match s {
+            Ok(st) => st
+                .parse::<i64>()
+                .map_err(|err| format!("Failed to parse int: {}", err)),
+            Err(_) => Err(String::from("Failed to read line.")),
+        })
+        .collect();
+
+    match elements {
+        Ok(ref v) => Ok(v.into_iter().sum()),
+        Err(s) => Err(s),
+    }
 }
